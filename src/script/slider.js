279431003bfb69952d01d3239c37fb503e7d51ff
usrlab22n2 <usrlab22n2@academico1.eniac>
2024-09-12 16:15:23 -0300

    Atualizaçao da tela de sobre
    finalizei a responsividade da tela e comecei o slider mas falta ajustar uma pequena transcição

---
+++ src/script/slider.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.botao-card').forEach((item,index) => { 
+    document.querySelectorAll('.botao-card')[index].addEventListener('click', () => {
+
+        for(var i = 0; i < document.querySelectorAll('.botao-card').length; i++) {
+            if(i == index) {
+                document.querySelectorAll('.pratica-card')[i].classList.add('ativo')
+                document.querySelectorAll('.botao-card')[i].classList.add('ativo')
+            } else {
+                document.querySelectorAll('.pratica-card')[i].classList.remove('ativo')
+                document.querySelectorAll('.botao-card')[i].classList.remove('ativo')
+            }
+        }
+        
+    })
+})
