a12f8adc8c08f8e711bafbcea00de6f89cce281c
Giovani-Dantas-Braconi <giovanidbraconi@gmail.com>
2024-10-23 15:33:00 -0300

    Atualizado o esquema de pastas e linkagem com admin --backup

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Cores)
+admin.site.register(Servicos)
+admin.site.register(Imagens_beneficios_cores)
+admin.site.register(Ideal_para)
+admin.site.register(Estacao)
+admin.site.register(Beneficios)
+admin.site.register(Imagens_equipe)
--- Core/models.py
+++ Core/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 class Cores(models.Model):
     nome = models.CharField(max_length=255)
-    cor_principal = models.CharField(max_length=1000)
-    cor_detalhes = models.CharField(max_length=1000)
+    cor_principal = models.CharField(max_length=7)
+    cor_detalhes = models.CharField(max_length=7)
     
     def __str__(self):
         return self.nome
@@ -15,7 +15,11 @@ class Cores(models.Model):
 
 class Servicos(models.Model):
     cor = models.ForeignKey(Cores, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to=f'images/global/cores/{cor}/servicos')
+    
+    def gerar_rota(self, filename):
+        return f'static/images/global/cores/{self.cor.nome}/servicos/{filename}'
+    
+    imagem = models.ImageField(upload_to=gerar_rota)
     
     def __str__(self):
         return f'serviço da cor {self.cor.nome}'
@@ -23,14 +27,23 @@ class Servicos(models.Model):
 
 class Imagens_beneficios_cores(models.Model):
     cor = models.ForeignKey(Cores, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to=f'images/global/cores/{cor}/beneficios')
+    
+    def gerar_rota(self, filename):
+        return f'static/images/global/cores/{self.cor}/beneficios/{filename}'
+
+
+    imagem = models.ImageField(upload_to=gerar_rota)
     
     def __str__(self):
         return f'beneficio da cor {self.cor.nome}'
 
 class Ideal_para(models.Model):
     cor = models.ForeignKey(Cores, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to=f'images/global/cores/{cor}/ideal_para')
+    
+    def gerar_rota(self, filename):
+        return f'static/images/global/cores/{self.cor.nome}/ideal_para/{filename}'
+
+    imagem = models.ImageField(upload_to=gerar_rota)
     
     def __str__(self):
         return f'Ideal para da cor {self.cor.nome}'
@@ -41,7 +54,9 @@ class Ideal_para(models.Model):
 #Estações
 class Estacao(models.Model):
     nome = models.CharField(max_length=255)
-    logo = models.ImageField(upload_to=f'images/estacoes/{nome}/logo')
+    def gerar_rota(self, filename):
+        return f'static/images/estacoes/{self.nome}/logo/{filename}'
+    logo = models.ImageField(upload_to=gerar_rota)
     cor = models.ForeignKey(Cores, on_delete=models.CASCADE)
     descricao_estacao = models.TextField(max_length=2000)
     texto_funcionamento = models.TextField(max_length=1000)
@@ -64,18 +79,22 @@ class Estacao(models.Model):
 
 
 class Beneficios(models.Model):
-    estacao = models.ForeignKey(Estacao, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to=f'images/estacoes/{estacao.name}/beneficios')
+    cor = models.ForeignKey(Cores, on_delete=models.CASCADE)
+    def gerar_rota(self, filename):
+        return f'static/images/cores/{self.cor.nome}/beneficios/{filename}'
+    imagem = models.ImageField(upload_to=gerar_rota)
 
     def __str__(self):
-        return f'Imagem de beneficio da estação {self.estacao.nome}'
+        return f'Imagem de beneficio da estação {self.cor.nome}'
 
 
 
 
 class Imagens_equipe(models.Model):
     estacao = models.ForeignKey(Estacao, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to=f'images/estacoes/{estacao.name}/equipe')
+    def gerar_rota(self, filename):
+        return f'static/images/estacoes/{self.estacao.nome}/equipe/{filename}'
+    imagem = models.ImageField(upload_to=gerar_rota)
     alt = models.CharField(max_length=100)
 
     def __str__(self):
