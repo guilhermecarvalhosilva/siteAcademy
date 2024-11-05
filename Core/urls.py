08c0c1d99d5cca8b08510bf4df65e23d38f432f1
Giovani-Dantas-Braconi <giovanidbraconi@gmail.com>
2024-11-05 15:33:12 -0300

    Teste atualizado, lógica do for de estações funcionando, esperando att od models para tornar mais intuitivo --backup

--- Core/urls.py
+++ Core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Sobre, Contato, DetalhesEstacaoView, Desenvolvedores
+from .views import Sobre, Contato, DetalhesEstacaoView, Desenvolvedores, TesteView
 
 
 app_name = "Core"
@@ -9,4 +9,5 @@ urlpatterns = [
     path('contato/', Contato.as_view(), name='contato'),
     path('estacao/<str:nome>/', DetalhesEstacaoView.as_view(), name='detalhes_estacao'),
     path('desenvolvedores/', Desenvolvedores.as_view(), name='desenvolvedores'),
+    path('teste/<str:nome>/', TesteView.as_view(), name='teste_view'),
 ]
--- Core/views.py
+++ Core/views.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView
-from .models import Estacao, Paleta_Core, Servico, Beneficio, Imagens_equipe
+from .models import Estacao, Paleta_Core, Servico, Beneficio, Imagens_equipe, Imagens_beneficios_core, Ideal_para
 
 class Homepage(TemplateView):
     template_name = 'entrada/index.html'
@@ -35,8 +35,30 @@ class DetalhesEstacaoView(DetailView):
         
         # Filtra os serviços que têm a mesma cor da estação
         context['servicos'] = Servico.objects.filter(cor=estacao_atual.cor)[:3]
-        context['beneficios'] = Beneficio.objects.all()[:3]
+        context['beneficios'] = Beneficio.objects.filter(cor=estacao_atual.cor)[:3]
         
         return context
 
     
+
+class TesteView(TemplateView):
+    template_name = 'entrada/teste.html'
+    
+    def get_object(self):
+        nome_estacao = self.kwargs.get('nome')
+        return Estacao.objects.get(nome=nome_estacao)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Pega a estação atual
+        estacao_atual = self.get_object()        
+        # Carregar instâncias de cada modelo
+        context['estacoes'] = Estacao.objects.all()
+        context['servicos'] = Servico.objects.filter(cor=estacao_atual.cor)[:3]
+        context['beneficios'] = Beneficio.objects.filter(cor=estacao_atual.cor)[:3]
+        context['imagens_equipes'] = Imagens_equipe.objects.all()
+        context['imagens_beneficios_cores'] = Beneficio.objects.filter(cor=estacao_atual.cor)[:3]
+        context['ideais_para'] = Ideal_para.objects.all()
+        
+        return context
