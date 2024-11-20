ad05ee35b820e91f1b929956ec43d885f612da9b
Giovani-Dantas-Braconi <giovanidbraconi@gmail.com>
2024-11-20 18:54:42 -0300

    Arrumando dashboard - paginador ainda faltando, inspec e delete adicionados, falta selecionador de estação, lógicas, paginador, estilizar input de pesquisa, e filtros em teste --backup

---
+++ static/script/pages/dashboard/list_projects/dashboard.js
@@ -0,0 +1,32 @@
+//elements 
+const btnToggler = window.document.querySelector(".navbar-toggler"); 
+const inputSearch = window.document.querySelector(".navbar-search"); 
+const navbar = window.document.querySelector(".navbar");
+
+//events
+btnToggler.addEventListener('click', () => {
+    navbar.classList.toggle('active'); 
+});
+
+inputSearch.addEventListener('click', () => {
+    if(!navbar.classList.contains("active")) {
+        navbar.classList.add('active'); 
+    }
+});
+  // Barra de Pesquisa 
+document.getElementById('project-search').addEventListener('input', function() {
+    const searchTerm = this.value.toLowerCase();
+    const rows = document.querySelectorAll('table tbody tr');
+
+    rows.forEach(row => {
+        const projectName = row.cells[1].textContent.toLowerCase(); // Obtém o nome do projeto
+
+        // Mostra a linha se o nome do projeto contém o termo de pesquisa
+        if (projectName.includes(searchTerm)) {
+            row.style.display = ''; // Exibe a linha
+        } else {
+            row.style.display = 'none'; // Oculta a linha
+        }
+    });
+});
+
